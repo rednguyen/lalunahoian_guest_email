e39b8331700bb6db913e3af5c71941c6dae25e21
rednguyen <tvn004@ucsd.edu>
2023-12-09 13:10:42 -0600

    change api.js

--- api.js
+++ api.js
@@ -9,7 +9,9 @@ var cors = require('cors');
 var app = express();
 var router = express.Router();
 var data = [];
-var fileName = today.getDate() + "-" + today.getMonth() + "-" + today.getFullYear() + "_lalunahoian.xlsx";
+var dataBooking = [];
+var fileName = today.getDate() + "-" + (today.getMonth()+1) + "-" + today.getFullYear() + "_lalunahoian.xlsx";
+var fileNameBooking = today.getDate() + "-" + (today.getMonth()+1) + "-" + today.getFullYear() + "_lalunahoian_booking.xlsx";
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -65,11 +67,11 @@ router.route('/').get((req, res) => {
 
 var port = process.env.PORT || 8090;
 app.listen((port), () => {
-    console.log("Generating file: " + fileName);
-     dboperations.getDepartureGuests().then(result => {
+    dboperations.getDepartureGuests().then(result => {
+        console.log("Generating file: " + fileName);
         data = result[0];
         data.push({Name: 'Red Nguyen', Email: 'rednguyen1997@gmail.com'});
-        data.push({Name: 'Nancy Ha', Email: 'nancy.ha@lalunahoian.com'});
+        data.push({Name: 'Nancy Ha', Email: 'thaonguyen010295@gmail.com'});
         console.log(data);
         const wb = new xl.Workbook();
         const ws = wb.addWorksheet("Laluna_Guest_Checkout");
@@ -88,5 +90,32 @@ app.listen((port), () => {
         })
         wb.write(fileName);
         console.log("Completed!");
-    })
+        // process.exit(0);
+    });
+
+    dboperations.getDepartureGuestsFromBooking().then(result => {
+        console.log("Generating file: " + fileNameBooking);
+        dataBooking  = result[0];
+        dataBooking .push({Name: 'Red Nguyen', Email: 'rednguyen1997@gmail.com'});
+        dataBooking .push({Name: 'Nancy Ha', Email: 'thaonguyen010295@gmail.com'});
+        console.log(dataBooking );
+        const wb = new xl.Workbook();
+        const ws = wb.addWorksheet("Laluna_Guest_Checkout_Booking");
+        let colIndex = 1;
+        headerColumns.forEach((item) => {
+            ws.cell(1, colIndex++).string(item)
+        })
+
+        let rowIndex = 2;
+        dataBooking.forEach((item) => {
+        let columnIndex = 1;
+        Object.keys(item).forEach((colName) => {
+            ws.cell(rowIndex, columnIndex++).string(String(item[colName]).trimStart());
+        })
+        rowIndex++;
+        })
+        wb.write(fileNameBooking);
+        console.log("Completed!");
+        // process.exit(1);
+    });
 });  
--- dboperations.js
+++ dboperations.js
@@ -18,7 +18,7 @@ async function getDepartureGuests(){
     try{
         let pool = await sql.connect(config);
         let results = await pool.request().query(
-        "select  a.LastName Name, a.MailM as Email from folio a join ActiveFolio b on a.FolioNum = b.FolioNum where convert(date, a.DepartureDate) = CAST(GETDATE() AS DATE)"
+        "select LTRIM(FirstName + ' ' + LastName) as Name,MailM from folio  where convert(date, DepartureDate) = CAST(GETDATE() AS DATE) and checkInTime is not NULL and CreditCardHolder <> 'Booking.com'"
         );
         return results.recordsets;
     }
@@ -27,7 +27,22 @@ async function getDepartureGuests(){
     }
 }
 
+async function getDepartureGuestsFromBooking(){
+    try{
+        let pool = await sql.connect(config);
+        let results = await pool.request().query(
+        "select  LTRIM(FirstName + ' ' + LastName) as Name,MailM from folio  where convert(date, DepartureDate) = CAST(GETDATE() AS DATE) and checkInTime is not NULL and CreditCardHolder = 'Booking.com'"
+        );
+        return results.recordsets;
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+
 module.exports ={
     getArrivalGuests : getArrivalGuests,
-    getDepartureGuests: getDepartureGuests
+    getDepartureGuests: getDepartureGuests,
+    getDepartureGuestsFromBooking: getDepartureGuestsFromBooking
 }
